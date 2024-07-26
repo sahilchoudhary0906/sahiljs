09bafbef72a43268cc74de01ab7adb09dacd19db
sahilchoudhary0906 <161453549+sahilchoudhary0906@users.noreply.github.com>
2024-07-26 12:18:12 +0000

    datatype video

---
+++ basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict";//treat all JS code as newer version
+
+//alert (3+3)// we are using nodejs browser
+/*console.log(3+3);
+console.log("sahil" ) 
+*/
+let name="sahil"
+let age = 18
+let isLoggedIn = false 
+//number ==2 to power 53
+//bigint
+//string=> true false
+// null=> undefined
+// symbol=>unique
+//object
+console.log(typeof null);//object
