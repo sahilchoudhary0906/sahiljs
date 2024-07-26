5be423c6d070d980937f06c5a0a66babc5064eef
sahilchoudhary0906 <161453549+sahilchoudhary0906@users.noreply.github.com>
2024-07-26 10:52:48 +0000

    variable video

---
+++ basics/variable.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "sahil12@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accountId = 2//not allowed
+
+accountEmail = "sahil@gmail.com"
+accountPassword ="123333"
+accountCity = "chandigarh"
+console.table([accountId, accountEmail, accountPassword, accountCity])
