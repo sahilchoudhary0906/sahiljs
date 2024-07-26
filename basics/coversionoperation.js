0ead292e459a7800ceb8a3f2d44b83c012e38ed5
sahilchoudhary0906 <161453549+sahilchoudhary0906@users.noreply.github.com>
2024-07-26 12:47:11 +0000

    conversion video

---
+++ basics/coversionoperation.js
@@ -0,0 +1,20 @@
+let score = null
+//console.log(typeof score);
+/*console.log(typeof score);
+let valueIntNumber = Number(score)
+console.log(typeof valueIntNumber);*/
+let sahilIntNumber = Number(score)
+//console.log( typeof sahilIntNumber);
+
+//"33"=> 33
+//'33abc'=>nan
+//true=>1; false=>0
+let isLoggedIn= 0
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+// 1=true 0=false
+//""=false
+//"sahil"=true
+let sahilHey= 33
+let stringSahil= String(sahilHey)
+console.log(typeof stringSahil);
