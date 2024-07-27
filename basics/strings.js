fe893fb946767f3f539a688a3b01d3c4db9b60eb
sahilchoudhary0906 <161453549+sahilchoudhary0906@users.noreply.github.com>
2024-07-27 09:26:17 +0000

    new

---
+++ basics/strings.js
@@ -0,0 +1,7 @@
+const name= "sahil"
+const repoCount = 50
+//console.log(name + repoCount + "value");
+//console.log(`hello my name is ${name} and my repo count is${repoCount}`);
+const gameName = new String('sahil')
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('h'));
