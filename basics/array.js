339805b22f269cf1494f9dcf83adcd2e0918d392
sahilchoudhary0906 <161453549+sahilchoudhary0906@users.noreply.github.com>
2024-07-29 06:59:54 +0000

    array is done

---
+++ basics/array.js
@@ -0,0 +1,30 @@
+let myArray= [1,2,3,4,5,6]
+//console.log(myArray)
+// resizable
+//console.log(myArray[4]);
+const myHeros= ["sahil","gugi","bros"]
+const myArr2 = new Array (1,2,3,4,5)
+//console.log(myArr2.length);
+// array methods
+//myArr2.push(6,7)
+//console.log(myArr2);
+//myArr2.pop()
+//console.log(myArr2); 
+//myArr2.unshift(10)
+//myArr2.shift()
+//console.log(myArr2);
+//console.log(myArr2.includes(19));
+//console.log(myArr2.indexOf(4));
+//const newArray = myArr2
+//console.log(newArray);
+//console.log(typeof  newArray);
+//slice, splice
+console.log("A",myArr2);
+const nyn1 =myArr2.slice(1,3)
+console.log(nyn1);
+console.log("B",myArr2);
+const nyn2 =myArr2.splice(1,3)
+console.log("c",myArr2);
+console.log(nyn2);
+// splice manipulate the original array
+// slice maynot manipulate the original array
