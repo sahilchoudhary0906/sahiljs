470fd49ab923031acd402effd28826c1f394a33d
sahilchoudhary0906 <161453549+sahilchoudhary0906@users.noreply.github.com>
2024-07-30 09:02:26 +0000

    object is added new

---
+++ basics/obj3.js
@@ -0,0 +1,20 @@
+//const mySym=Symbol("mykey1")
+const course = {
+    name:"sahil",
+    age:12,
+    courseInstructor:"dr sudesh kumar",
+    class:"12"
+}
+const{courseInstructor:inst}=course;
+console.log(inst);
+//console.log(course);
+//course.greeting= function(){
+  //  console.log("hey my sahil");
+//}
+//console.log(course.greeting());
+//-----------------api things json------------
+{
+    "name": "hitesh",
+    "price":"free",
+   " coursename":"js"
+}
